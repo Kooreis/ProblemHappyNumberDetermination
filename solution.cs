b95650d6c817deffc891550af0c13ea7c55bcbf9
Kooreis <kooreis.official@gmail.com>
2024-04-23 09:48:26 -0500

    In this update, a conditional statement is added to check if the entered number is a 'happy number'. Depending on the result, a corresponding message is printed to the console.

--- solution.cs
+++ solution.cs
@@ -6,5 +6,14 @@ class Program
     {
         Console.Write("Enter a number: ");
         int number = Convert.ToInt32(Console.ReadLine());
+
+        if (IsHappyNumber(number))
+        {
+            Console.WriteLine(number + " is a happy number");
+        }
+        else
+        {
+            Console.WriteLine(number + " is not a happy number");
+        }
     }
 }
