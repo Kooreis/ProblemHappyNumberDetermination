201b3d3f51acd163c51730e1b2287e80b4ce084c
Kooreis <kooreis.official@gmail.com>
2024-04-23 09:48:26 -0500

    Created the main class and method. Added user input functionality to enter a number and check if it is a happy number.

--- solution.java
+++ solution.java
@@ -1,3 +1,11 @@
-import java.util.HashSet;
-import java.util.Scanner;
-import java.util.Set;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a number to check if it is a happy number:");
+        int num = scanner.nextInt();
+        if (isHappy(num)) {
+            System.out.println(num + " is a happy number");
+        } else {
+            System.out.println(num + " is not a happy number");
+        }
+    }
